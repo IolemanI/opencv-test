88450c944182e05bc68b91ee6fac0aa1ee4e0cb2
OleMan <aom-95@live.com>
2020-01-29 02:14:34 +0200

    add recognition to server

--- server.py
+++ server.py
@@ -5,8 +5,17 @@ from flask import Response
 import jsonpickle
 import numpy as np
 import cv2
+import pickle
 
 app = Flask(__name__)
+
+# load our serialized face embedding model from disk
+print("[INFO] loading face recognizer...")
+embedder = cv2.dnn.readNetFromTorch('resources/openface_nn4.small2.v1.t7')
+
+# load the actual face recognition model along with the label encoder
+recognizer = pickle.loads(open('resources/output/recognizer.pickle', "rb").read())
+le = pickle.loads(open('resources/output/le.pickle', "rb").read())
 
 @app.route('/')
 def index():
@@ -19,14 +28,42 @@ def get_campaigns():
     # convert string of image data to uint8
     nparr = np.fromstring(r.data, np.uint8)
     # decode image
-    img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+    face = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
     # do some fancy processing here....
-    blur = cv2.Laplacian(img, cv2.CV_64F).var()
-    cv2.imwrite(str(int(blur)) + ".jpg", img)
+    blur = cv2.Laplacian(face, cv2.CV_64F).var()
+    # cv2.imwrite(str(int(blur)) + ".jpg", face)
+
+    # construct a blob for the face ROI, then pass the blob
+    # through our face embedding model to obtain the 128-d
+    # quantification of the face
+    faceBlob = cv2.dnn.blobFromImage(
+        face, 
+        1.0 / 255, 
+        (96, 96),
+        (0, 0, 0), 
+        swapRB=True, 
+        crop=False
+    )
+    embedder.setInput(faceBlob)
+    vec = embedder.forward()
+
+    # perform classification to recognize the face
+    preds = recognizer.predict_proba(vec)[0]
+    j = np.argmax(preds)
+    proba = preds[j]
+    name = le.classes_[j]
 
     # build a response dict to send back to client
-    response = { 'message': 'image received. size={}x{}'.format(img.shape[1], img.shape[0]) }
+    response = { 
+        'image': {
+            'size': '{}x{}'.format(face.shape[1], face.shape[0]),
+        },
+        'face': {
+            'name': "{}".format(name),
+            'confidence': float(proba)
+        }
+    }
     # encode response using jsonpickle
     response_pickled = jsonpickle.encode(response)
 
